42c94addf7f4cdacb88c7318d2892fca20446f3b
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-15 17:53:07 +0900

    タスクの追加・一覧表示を実装

--- main.rb
+++ main.rb
@@ -15,14 +15,33 @@ class Task
   end
 end
 
-# task1 = Task.new
-# task2 = Task.new
-# task3 = Task.new
+class ToDo
+  def initialize
+    @tasks = []
+  end
+
+  def add(task)
+    puts "【追加】 [No.#{task.id}] #{task.title}:#{task.content}"
+    @tasks << task
+  end
+
+  def info
+    puts "*=*=*=*=*=*=*=*=* task *=*=*=*=*=*=*=*=*"
+    @tasks.each do |tas|
+      puts "[No.#{tas.id}] #{tas.title}:#{tas.content}"
+    end
+    puts "*=*=*=*=*=*=*=*=*=*=*=**=*=*=*=*=*=*=*=*"
+  end
+end
+
+
 
 task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
 task2 = Task.new(title: "仕事", content: "9時〜18時")
 task3 = Task.new(title: "買物", content: "卵, ヨーグルト")
 
-puts task1.info
-puts task2.info
-puts task3.info
+
+todo = ToDo.new
+todo.add(task1)
+todo.add(task2)
+todo.info
